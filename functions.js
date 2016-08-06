b6a378e9664ca9e6d36667698d636184c9bb6abb
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-08-05 21:39:44 -1000

    complete #1

--- functions.js
+++ functions.js
@@ -1,29 +1,32 @@
-/*If statements
+/*If statements 
 If statments are a way for our applications to take different paths through our code or make choices about what kind of behavior our application takes based on certain conditions or values in our variables. The variable or value that we are testing to be true is known as the condition.*/
+
 /*In Human: If this condition is true, do something.*/
 
-var isTurnedOn = false;
-function flipSwitch(){
-  if(isTurnedOn){
-    isTurnedOn = false;
-  }
-}
-/*If/Else Statements
+var isTurnedOn = false; 
+function flipSwitch(){   
+	if(isTurnedOn){
+		isTurnedOn = false;   
+	} 
+} 
+
+/*If/Else Statements 
 If/Else statments have the same behavior as our if statement above but we can add a secondary condition in the case that our if statement is not true to take another action instead. What if our light is already turned off when we flip the switch?
 
 In Human: If this condition is true, do something, else if it is false, do something else.*/
 
-var isTurnedOn = false;
-function flipSwitch(){
-  if(isTurnedOn){
-    isTurnedOn = false;
-  }
-  else{
-    isTurnedOn = true;
-  }
-}
-/*For loops
-Sometimes we want to loop over a collection of data stored in an Arrayor inspect each character in a String. Or we just want to execute a block of code a set number of times before continuing on with the execution of our program. For loops allow us to execute a block of code and at the end of each iteration of our for loop we will evaluate a condition to determine if our loop should run again similar to our if statment.
+var isTurnedOn = false; 
+function flipSwitch(){   
+	if(isTurnedOn){     
+		isTurnedOn= false;   
+	}else{     
+		isTurnedOn = true;   
+	} 
+} 
+
+/*For loops 
+Sometimes we want to loop over a collection of data stored in an Arrayor inspect each character in a String. Or we just want to execute a block of code a set number of times before continuing on with the execution of our program. For loops allow us to execute a block of code and at the end of each iteration of our for loop we will evaluate a condition to determine if our loop should run again similar to our if
+statment.
 
 Our for loop is made up of 3 parts: 1. Variable initialization 2. Termination condition 3. Afterthought
 
@@ -41,10 +44,17 @@ function makeCrazyLongString(string){
     outputString = outputString.concat(string);
   }
 }
-/*makeCrazyLongString('taco');
+
+/*makeCrazyLongString('taco'); 
 Function - youGetTaco
-Write a function that takes a single String parameter called action. If the value passed into our function is eathave the function return the String value EAT TACOS.
+Write a function that takes a single String parameter called action. If the value passed into our function is eat have the function return the String value EAT TACOS.
 */
+function youGetTaco(action){
+	if(action==='eat'){
+		return 'EAT TACOS';
+	}
+}
+console.log(youGetTaco('eat'));
 
 
 /*Function - isNumberGreaterThan
